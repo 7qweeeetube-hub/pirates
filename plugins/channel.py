7b4e3b1362f2f70bd1df70564e48637d8b038fc0
tamilmoviesdownload <ajayneymar14@gmail.com>
2026-07-02 21:01:34 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -7,8 +7,8 @@ media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
-    """Media Handler"""
     media = getattr(message, message.media.value, None)
     if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
         media.caption = message.caption
-        await save_file(media)
+        # Pass chat.id and message.id
+        await save_file(media, message.chat.id, message.id)
